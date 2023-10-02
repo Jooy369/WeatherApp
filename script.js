68116ae66a3084eb724b27a298e8f5cc6d902973
Jooy369 <agus03548@gmail.com>
2023-10-02 22:06:49 +0700

    sedikit perubahan pada script.js

--- script.js
+++ script.js
@@ -3,64 +3,65 @@ class WeatherApp {
         this.API_key = "03cb70cb15536e729210c0069772c51e";
     }
     
-    // Metode untuk mengambil koordinat berdasarkan nama kota
-    geocoding(city) {
+    async geocoding(city) {
         const url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${this.API_key}`;
         
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
             console.log(data);
             let lat = data[0].lat;
             let lon = data[0].lon;
             
-            // Memanggil metode untuk mendapatkan cuaca saat ini
-            this.getCurrentWeather(lat, lon);
-        });
+            await this.getCurrentWeather(lat, lon);
+        } catch (error) {
+            console.error('Terjadi kesalahan:', error);
+        }
     }
     
-    // Metode untuk mendapatkan data cuaca saat ini
-    getCurrentWeather(lat, lon) {
+    async getCurrentWeather(lat, lon) {
         const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.API_key}&units=metric`;
         
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
             let city = data.name;
             let country = data.sys.country;
             let icon = data.weather[0].icon;
+            let icon_ui = `https://openweathermap.org/img/wn/${icon}@4x.png`;
             let status = data.weather[0].description;
             let temperature = parseInt(data.main.temp);
-            let date = new Date(data.dt*1000 + data.timezone);
+            let date = new Date(data.dt * 1000 + data.timezone);
             let time = date.toLocaleTimeString('en-US', { minute: '2-digit', hour: '2-digit'});
             let day = date.toLocaleDateString('en-US', { weekday: 'long' });
             
-            // Menyusun data cuaca dalam objek
             const weatherdata = {
                 city: city, 
-                country: country, 
-                icon: icon, 
+                country: country,
+                icon: icon,
+                icon_ui: icon_ui, 
                 status: status, 
                 temperature: temperature, 
                 time: time, 
                 day: day
             };
             
-            // Memanggil metode untuk memperbarui tampilan cuaca
-            this.updateCurrentWeather(weatherdata);
-        });
+            await this.updateCurrentWeather(weatherdata);
+        } catch (error) {
+            console.error('Terjadi kesalahan getweather:', error);
+        }
     }
     
     // Metode untuk memperbarui tampilan cuaca
     updateCurrentWeather(weatherdata) {
-        const {city, country, icon, status, temperature, time, day} = weatherdata;
+        const {city, country, icon, icon_ui, status, temperature, time, day} = weatherdata;
         
         const currentWeather = document.getElementById('current_weather');
         currentWeather.innerHTML = `
-            <div class="container bg-dark text-center col-auto rounded-4 overflow-hidden text-white font-monospace" style="font-size: 12px;">
+            <div class="container bg-dark text-center rounded-4 overflow-hidden text-white font-monospace" style="font-size: 12px;">
                 <div class="row">
                     <div class="col-12 bg-info" id="bg_icon">
-                        <img src="https://openweathermap.org/img/wn/${icon}@4x.png" alt="weather_icon">
+                        <img src="${icon_ui}" alt="weather_icon">
                     </div>
                 </div>
                 <div class="row">
@@ -81,7 +82,7 @@ class WeatherApp {
         if (icon[2] == 'n') {
             const bg = document.getElementById('bg_icon');
             bg.classList.remove('bg-info');
-            bg.classList.add('bg-primary', 'bg-opacity-75');
+            bg.classList.add('bg-primary', 'bg-opacity-50');
         }
     }
 }
@@ -91,10 +92,20 @@ const weatherApp = new WeatherApp();
 
 // Menambahkan event listener untuk tombol pencarian
 const searchButton = document.getElementById('btn_search_city');
-searchButton.addEventListener('click', () => {
+searchButton.addEventListener('click', async () => {
     const city = document.getElementById('city_name').value;
     if (city) {
-        weatherApp.geocoding(city);
+        const currentWeather = document.getElementById('current_weather');
+        currentWeather.innerHTML = `
+                <div class="container rounded-4 d-flex justify-content-center align-items-center" style="height: 200px" id="loading">
+                    <div class="spinner-border"></div>
+                </div> `;
+
+        
+        
+        await weatherApp.geocoding(city);
+        
+        console.log(currentWeather);
     } else {
         alert('invalid input!');
     }
